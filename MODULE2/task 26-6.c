2134e074c4c61172345fc56918e3262495d59f39
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-06-26 11:53:10 +0530

    Add files via upload

---
+++ MODULE2/task 26-6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Kumar Rathod");
+    printf("\n Rajkot");
+    printf("\n 18-08-06");
+    printf("\n bsc IT");
+    
+    getch();
+    
+}

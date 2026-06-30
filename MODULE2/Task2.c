4bddc791bac1df06a65f961beaeadf99d5bc1ed2
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-06-30 17:19:34 +0530

    Add files via upload

---
+++ MODULE2/Task2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a=9;
+  int b=7;
+ 
+  a+=b;
+ printf("\n %d",a);
+ a-=b;
+    printf("\n %d",a);
+
+    a*=b;
+    printf("\n %d",a);
+
+    a/=b;
+    printf("\n %d",a);
+
+    a%=b;
+    printf("\n %d",a);
+    
+    getch();
+
+}

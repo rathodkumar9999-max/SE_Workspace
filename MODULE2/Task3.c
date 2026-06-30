316ccc5eb046080d220d9aa4e3beea1e2e7fd1d8
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-06-30 17:34:16 +0530

    Add files via upload

---
+++ MODULE2/Task3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (){
+
+int num= 6;
+
+int square=num*num;
+printf("\n num of sqaure is :- %d",square);
+
+int cube= num*num*num;
+printf("\n num of cube is :- %d",cube);
+return 0;
+
+}

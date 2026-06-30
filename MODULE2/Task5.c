0db741b4089c71bb5290c90ab760c13d6db3e4f8
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-06-30 17:44:47 +0530

    Add files via upload

---
+++ MODULE2/Task5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int base=6;
+    int height=8;
+    float area=0.5*base*height;
+    printf("area of triangle is :- %f",area);
+getch();
+
+}

31f9b85b16ff9604cd2cd3fa593d86f379448029
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-06-30 17:50:52 +0530

    Add files via upload

---
+++ MODULE2/Task7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (){
+    
+int sub1=165;
+int sub2=9;
+int sub3=86;
+int sub4=88;
+int sub5=90;
+
+int sum =sub1+sub2+sub3+sub4+sub5;
+printf("\n Sum of 5 sub markes :- %d",sum);
+
+float per =sum/5;
+printf("\n Percentage is :- %f",per);
+
+getch();
+}

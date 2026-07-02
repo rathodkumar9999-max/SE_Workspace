f98582324bf2e566fa7f8bd03f35e97fa95d54a1
rathodkumar9999-max <rathod.kumar9999@gmail.com>
2026-07-02 17:22:46 +0530

    Add files via upload

---
+++ MODULE2/Task8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int num=-21;
+    
+    if(num>0)
+    {
+        printf("%d :- Num is Positive ",num);
+    }
+    else
+    {
+        printf("%d :- Num is Negative",num);
+    }
+    getch();
+}
